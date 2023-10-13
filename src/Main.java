d4b6b5ed16ddca80f8c714149354aa9d1d12cf01
Nistel <duz3000@gmail.com>
2023-10-13 20:10:05 +0300

    feature add class for work

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(Считать все месячные отчёты);
+        m.202101.csv
+        m.202102.csv
+        m.202103.csv
+
+        FileReader fileReader = new FileReader();
+
+        //fileName – имя файла в папке resources
+        ArrayList<String> lines = fileReader.readFileContents("m.202101.csv");
+
+        Scanner scanner = new Scanner(System.in);
+        String[] lineContents = line.split(",");
+        while (true) {
+            // Вывод доступных пунктов меню в консоль
+            int userInput = scanner.nextInt();
+
+        System.out.println(Считать годовой отчёт
+        y.2021.csv
+
+        System.out.println Сверить отчёты
+
+        System.out.println Вывести информацию обо всех месячных отчётах
+
+        System.out.println Вывести информацию о годовом отчёте
+
+        System.out.println Выйти
+
     }
 }
 
