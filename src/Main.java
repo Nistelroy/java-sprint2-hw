eec4fc2d8d594c096674daedc5cc29249496d6fd
Nistel <duz3000@gmail.com>
2023-10-14 09:06:40 +0300

    feature add menu

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Считать все месячные отчёты);
-        m.202101.csv
-        m.202102.csv
-        m.202103.csv
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Считать все месячные отчёты");
+        //  m.202101.csv
+        //   m.202102.csv
+        //   m.202103.csv
 
         FileReader fileReader = new FileReader();
 
         //fileName – имя файла в папке resources
         ArrayList<String> lines = fileReader.readFileContents("m.202101.csv");
 
-        Scanner scanner = new Scanner(System.in);
-        String[] lineContents = line.split(",");
+
+
+    //    String[] lineContents = line.split(",");
         while (true) {
             // Вывод доступных пунктов меню в консоль
             int userInput = scanner.nextInt();
 
-        System.out.println(Считать годовой отчёт
-        y.2021.csv
+            if (userInput = 1){
+                System.out.println("Считать годовой отчёт");
+            //    y.2021.csv
+            }
 
-        System.out.println Сверить отчёты
+            else if (userInput = 2) {
+                System.out.println("Сверить отчёты");
+            }
 
-        System.out.println Вывести информацию обо всех месячных отчётах
+            else if (userInput = 3) {
+            System.out.println("Вывести информацию обо всех месячных отчётах");
+            }
 
-        System.out.println Вывести информацию о годовом отчёте
+            else if (userInput = 4) {
+            System.out.println("Вывести информацию о годовом отчёте");
+            }
 
-        System.out.println Выйти
+            else if (userInput = 5) {
+            System.out.println("Выйти");
+                scanner.close();
+                return;
+            }
+            else System.out.println("нет команды");
 
+
+        }
     }
 }
 
